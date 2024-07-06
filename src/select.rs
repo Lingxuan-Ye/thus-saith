7ff50847f418006f5ea8ce7a3af3702c8b38d511
Lingxuan Ye <love@lingxuan.io>
2024-07-07 01:51:02 +0800

    fix: correct behaviors on specified corner cases

--- src/select.rs
+++ src/select.rs
@@ -1,4 +1,5 @@
 use crate::config::{Config, Quote};
+use anyhow::{ensure, Result};
 use rand::Rng;
 use rand_distr::WeightedIndex;
 
@@ -10,18 +11,19 @@ struct NormalizedQuote<'a> {
 pub struct Selector;
 
 impl Selector {
-    pub fn select(config: &Config) -> Option<&str> {
-        let normalized = Self::normalize(&config.quotes);
-        let distribution = WeightedIndex::new(normalized.iter().map(|quote| quote.weight)).ok()?;
-        let mut rng = rand::thread_rng();
-        let index = rng.sample(distribution);
-        Some(normalized[index].content)
+    pub fn select(config: &Config) -> Result<&str> {
+        let normalized = Self::normalize(&config.quotes)?;
+        let weights = normalized.iter().map(|quote| quote.weight);
+        let distribution = WeightedIndex::new(weights).expect("will never fail");
+        let index = rand::thread_rng().sample(distribution);
+        Ok(normalized[index].content)
     }
 
-    fn normalize(quotes: &Vec<Quote>) -> Vec<NormalizedQuote<'_>> {
+    fn normalize(quotes: &Vec<Quote>) -> Result<Vec<NormalizedQuote<'_>>> {
         let mut normalized = Vec::with_capacity(quotes.len());
         let mut unweighted = Vec::new();
         let mut total_weight = 0.0;
+
         for quote in quotes {
             match quote.weight {
                 None => {
@@ -43,15 +45,22 @@ impl Selector {
                 }
             }
         }
+
+        ensure!(!normalized.is_empty(), "no valid quotes found");
+
         if !unweighted.is_empty() {
             let average_weight = total_weight / normalized.len() as f64;
             for quote in unweighted {
+                total_weight += average_weight;
                 normalized.push(NormalizedQuote {
                     weight: average_weight,
                     content: &quote.content,
                 });
             }
         }
-        normalized
+
+        ensure!(total_weight.is_finite(), "total weight too large");
+
+        Ok(normalized)
     }
 }
