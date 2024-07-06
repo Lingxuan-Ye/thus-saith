59d5ed5c84a051b1214f217ab6fa348fe73dd18b
Lingxuan Ye <love@lingxuan.io>
2024-07-07 06:46:34 +0800

    feat: define and implement `ExitOnError`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use cli::Cli;
+use config::Config;
+use exit::ExitOnError;
+use sanity::{NonNegFinite, PosFinite};
+use select::Selector;
+use std::io::stdout;
+use tokenizer::Tokenizer;
+use typist::Typist;
+
 mod cli;
 mod config;
 mod exit;
@@ -7,5 +16,14 @@ mod tokenizer;
 mod typist;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = Cli::new().get_matches();
+
+    let mean: PosFinite = *matches.get_one("mean").expect("will never fail");
+    let std_dev: NonNegFinite = *matches.get_one("std-dev").expect("will never fail");
+
+    let config = Config::load().exit_on_error();
+    let quote = Selector::select(&config).exit_on_error();
+    let chars = Tokenizer::tokenize(quote);
+    let mut typist = Typist::with_chars_per_min(mean, std_dev);
+    typist.type_out(chars, stdout()).exit_on_error();
 }
