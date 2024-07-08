f12451f2fd697cebdbdb9d8a211172ad7d261d07
Lingxuan Ye <love@lingxuan.io>
2024-07-08 08:11:12 +0800

    fix!: replace erroneous implementation

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,4 @@
-use crate::sanity::{NonNegFinite, PosFinite};
-use anyhow::{anyhow, Result};
-use clap::{command, Arg, ArgMatches, Command};
+use clap::{command, value_parser, Arg, ArgMatches, Command};
 
 pub struct Cli(Command);
 
@@ -8,27 +6,18 @@ impl Cli {
     pub fn new() -> Self {
         let command = command!().args([
             Arg::new("mean")
-                .short('m')
                 .long("mean")
                 .value_name("NUMBER")
-                .value_parser(|s: &str| -> Result<PosFinite> {
-                    let value: f64 = s.parse().map_err(|_| anyhow!("invalid literal"))?;
-                    PosFinite::build(value)
-                })
-                .default_value("2000")
-                .help("The average number of characters typed per minute"),
+                .value_parser(value_parser!(f64))
+                .default_value("100")
+                .help("The average time (in milliseconds) taken per character"),
             Arg::new("std-dev")
-                .short('s')
                 .long("std-dev")
                 .value_name("NUMBER")
-                .value_parser(|s: &str| -> Result<NonNegFinite> {
-                    let value: f64 = s.parse().map_err(|_| anyhow!("invalid literal"))?;
-                    NonNegFinite::build(value)
-                })
-                .default_value("4000")
-                .help("The standard deviation of the number of characters typed per minute"),
+                .value_parser(value_parser!(f64))
+                .default_value("100")
+                .help("The standard deviation (in milliseconds) of the time taken per character"),
         ]);
-
         Self(command)
     }
 
