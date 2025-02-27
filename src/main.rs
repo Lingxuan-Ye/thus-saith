01f976e1d7502a858214f6c347e36779a9c18839
Lingxuan Ye <love@lingxuan.io>
2025-02-27 21:24:08 +0800

    refactor: improve readability for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use cli::Cli;
 use config::Config;
 use eoe::ExitOnError;
@@ -15,23 +16,33 @@ mod signal;
 mod tokenizer;
 mod typist;
 
-fn main() {
+fn execute() -> Result<()> {
     let matches = Cli::new().get_matches();
 
     let file: Option<&PathBuf> = matches.get_one("config");
     let config = match file {
-        Some(file) => Config::load_from_file(file).exit_on_error(),
-        None => Config::load().exit_on_error(),
+        Some(file) => Config::load_from_file(file)?,
+        None => Config::load()?,
     };
 
     set_handler_for_sigint(config.messages.interrupt);
 
-    let mean: f64 = *matches.get_one("mean").expect("will never fail");
-    let std_dev: f64 = *matches.get_one("std-dev").expect("will never fail");
-    let mut typist = Typist::with_millis_per_char(mean, std_dev).exit_on_error();
+    let Some(&mean): Option<&f64> = matches.get_one("mean") else {
+        unreachable!()
+    };
+    let Some(&std_dev): Option<&f64> = matches.get_one("std-dev") else {
+        unreachable!()
+    };
+    let mut typist = Typist::with_millis_per_char(mean, std_dev)?;
 
-    let quote = Selector::select(&config.quotes).exit_on_error();
+    let quote = Selector::select(&config.quotes)?;
     let chars = Tokenizer::tokenize(quote);
     let output = stdout();
-    typist.type_out(chars, output).exit_on_error();
+    typist.type_out(chars, output)?;
+
+    Ok(())
+}
+
+fn main() {
+    execute().exit_on_error();
 }
